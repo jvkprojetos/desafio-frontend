4448d05f72189494ff844f75a1d6eba0379ef4f0
João Vitor <jvkprojetos@outlook.com>
2023-08-03 19:16:57 -0300

    Iniciando desenvolvimento componente search e incluindo no header.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeaderComponent } from './components/header/header.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    SearchComponent
   ],
   imports: [
     CommonModule,
