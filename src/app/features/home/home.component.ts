9cf80770060714d793e8bf3861401de001583818
João Vitor <jvkprojetos@outlook.com>
2023-08-04 00:27:18 -0300

    Removendo codigo não utilizado.

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Snippet } from "src/app/core/models/Snippet";
-import { VideoApiService } from 'src/app/core/services/video-api.service';
 
 @Component({
   selector: 'app-home',
@@ -9,19 +7,8 @@ import { VideoApiService } from 'src/app/core/services/video-api.service';
 })
 export class HomeComponent implements OnInit {
 
-  mostPopularVideos: Array<Snippet> = [];
+  constructor() { }
 
-  constructor(private videoApiService: VideoApiService) { }
+  ngOnInit(): void { }
 
-  ngOnInit(): void {
-    this.videoApiService.getMostPopular().subscribe((items) => {
-      items.items?.forEach((item) => {
-        if(item.snippet)
-          this.mostPopularVideos.push(item.snippet);
-      })
-    });
-  }
-
-  search(value: string) {
-  }
 }
