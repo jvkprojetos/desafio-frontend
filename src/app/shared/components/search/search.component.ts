aa1235cc18a94eb7647f1b4e5f829d79fa421911
João Vitor <jvkprojetos@outlook.com>
2023-08-04 19:22:08 -0300

    Criando historico de buscas.

--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -7,13 +7,34 @@ import { Router } from '@angular/router';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-    
+  
+  filterText: string = ''
+  showHistory: boolean = false;
+  list: Array<string> = [];
+
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.list.push('teste1')
+    this.list.push('teste2')
+    this.list.push('teste3')
   }
 
   searchClick(value: string) {   
     this.router.navigate(['results', value])
+  }
+
+  onClickInput(){
+    if(this.showHistory) {
+      this.showHistory = false;
+      return;
+    }
+
+    this.showHistory = true;
+  }
+
+  onClickSelected(string: string){
+    this.filterText = string;
+    this.showHistory = false;
   }
 }
